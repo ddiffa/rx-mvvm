d0625ea6bf8570f22a9a85654a0f6276099c28d4
ddiffa <ddiffa2@gmail.com>
2020-03-12 04:19:55 +0700

    add instrumentation testing

--- app/src/main/java/com/example/android/databinding/basicsample/BaseApplication.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/BaseApplication.kt
@@ -3,7 +3,6 @@ package com.example.android.databinding.basicsample
 import android.app.Application
 import com.example.android.databinding.basicsample.di.appModule
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
 class BaseApplication : Application() {
 
