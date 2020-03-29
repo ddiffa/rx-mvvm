a94f04357659185aa2a2897da2792d9560049f8c
ddiffa <ddiffa2@gmail.com>
2020-03-29 18:59:00 +0700

    add scheduler provider

--- app/src/main/java/com/example/android/databinding/basicsample/utils/SchedulerProviders.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/utils/SchedulerProviders.kt
@@ -1,10 +1,32 @@
 package com.example.android.databinding.basicsample.utils
 
 import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
 interface SchedulerProviders {
-    fun ui(): Scheduler
-    fun io(): Scheduler
-    fun computation() : Scheduler
+    fun io(): Scheduler?
+    fun computation(): Scheduler?
+    fun ui(): Scheduler?
+
+    companion object {
+        @JvmStatic
+        val DEFAULT: SchedulerProviders = object : SchedulerProviders {
+            override fun io(): Scheduler? = Schedulers.io()
+
+            override fun computation(): Scheduler? = Schedulers.computation()
+            override fun ui(): Scheduler? = AndroidSchedulers.mainThread()
+
+
+        }
+
+        @JvmStatic
+        val TEST_SCHEDULER: SchedulerProviders = object : SchedulerProviders {
+            override fun io(): Scheduler? = Schedulers.trampoline()
+
+            override fun computation(): Scheduler? = Schedulers.trampoline()
+            override fun ui(): Scheduler? = Schedulers.trampoline()
+        }
+    }
 }
